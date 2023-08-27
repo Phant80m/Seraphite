3919a0925ba24374880680d89272f60368ea5541
phant80m <black.pandazayd@gmail.com>
2023-08-27 11:23:41 +1000

    requires paru aur for installing dependencies!

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -2,14 +2,22 @@ use crate::linker::ToPathbuf;
 use owo_colors::OwoColorize;
 use std::fs;
 use std::process::{Command, Stdio};
-// use which::which;
+use which::which;
 
 pub fn sync_deps(no_confirm: bool) {
     if !"dotfiles/dependencies".home_path().exists() {
         println!("{} Dependency file not found!", "[  ]".red().bold(),);
         return;
     }
-    // let paru = which("paru").unwrap_or_default();
+    let paru = which("paru").unwrap_or_default();
+    if !paru.exists() {
+        println!(
+            "{} Paru not found in path, is it installed?",
+            "[  ]".red().bold(),
+        );
+
+        return;
+    }
     let file = fs::read_to_string("dotfiles/dependencies".home_path()).unwrap();
     if no_confirm {
         for line in file.lines() {
