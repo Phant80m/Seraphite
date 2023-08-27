39e41c328c17de83d3d237231ff3c9cde0b73be3
phant80m <black.pandazayd@gmail.com>
2023-08-27 11:51:22 +1000

    better backup

--- backup/2023-08-27_11-37-19/hypr/scripts/screenshot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-output= grimblast save area ~/.trash/tmp.png | ascella upload ~/.trash/tmp.png | grep -o 'https://i.indium.live/[^ ]*' | cut -d ' ' -f1 | wl-copy
-
-notify-send "Screenshot taken" "$output"
-
